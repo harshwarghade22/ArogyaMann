24a31a173e408fed53a233a42de6cc7bd89f4c1b
harshwarghade22 <harsh.warghade22@spit.ac.in>
2024-10-22 15:00:42 +0530

    settings changed

--- server/server/settings.py
+++ server/server/settings.py
@@ -96,9 +96,10 @@ WSGI_APPLICATION = 'server.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 
 # Password validation
